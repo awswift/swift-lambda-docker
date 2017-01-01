a489dcdd6862eaa83c8df6a0902b0b550023aa40
Aidan Steele <aidan.steele@glassechidna.com.au>
2017-01-01 19:20:27 +1100

    uncomment

--- src/packer.sh
+++ src/packer.sh
@@ -10,16 +10,15 @@ TAG="0.1"
 
 docker build -t awswift/swift-deps:$TAG base
 
-# docker build -t awswift/swift-compiler:$TAG compiler
-# docker run --cidfile=cid awswift/swift-compiler:$TAG true
-# CID=$(cat cid)
-# docker cp $CID:/swiftbuild/package/swift-linux-x86_64-amzn.tgz .
-# docker rm $CID
+docker build -t awswift/swift-compiler:$TAG compiler
+docker run --cidfile=cid awswift/swift-compiler:$TAG true
+CID=$(cat cid)
+docker cp $CID:/swiftbuild/package/swift-linux-x86_64-amzn.tgz .
+docker rm $CID
 
-# docker build -t awswift/swiftda:$TAG swiftda
-# docker tag awswift/swiftda:$TAG awswift/swiftda:latest
+docker build -t awswift/swiftda:$TAG swiftda
+docker tag awswift/swiftda:$TAG awswift/swiftda:latest
 
 docker login -u $DOCKERHUB_USER -p $DOCKERHUB_PASSWORD
-docker push awswift/swift-deps:$TAG
-# docker push awswift/swiftda:$TAG
-# docker push awswift/swiftda:latest
+docker push awswift/swiftda:$TAG
+docker push awswift/swiftda:latest
