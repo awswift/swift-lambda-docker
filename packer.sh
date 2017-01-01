cd41e9bd563a992e8ec0c93918c28f0b253cfc1b
Aidan Steele <aidan.steele@glassechidna.com.au>
2017-01-01 18:28:59 +1100

    Comment out code temporarily for quicker validation of CI

--- packer.sh
+++ packer.sh
@@ -8,14 +8,14 @@ TAG="0.1"
 
 docker build -t awswift/swift-deps:$TAG base
 
-docker build -t awswift/swift-compiler:$TAG compiler
-docker run --cidfile=cid awswift/swift-compiler:$TAG true
-CID=$(cat cid)
-docker cp $CID:/swiftbuild/package/swift-linux-x86_64-amzn.tgz .
-docker rm $CID
+# docker build -t awswift/swift-compiler:$TAG compiler
+# docker run --cidfile=cid awswift/swift-compiler:$TAG true
+# CID=$(cat cid)
+# docker cp $CID:/swiftbuild/package/swift-linux-x86_64-amzn.tgz .
+# docker rm $CID
 
-docker build -t awswift/swiftda:$TAG swiftda
-docker tag awswift/swiftda:$TAG awswift/swiftda:latest
+# docker build -t awswift/swiftda:$TAG swiftda
+# docker tag awswift/swiftda:$TAG awswift/swiftda:latest
 
-docker push awswift/swiftda:$TAG
-docker push awswift/swiftda:latest
+# docker push awswift/swiftda:$TAG
+# docker push awswift/swiftda:latest
