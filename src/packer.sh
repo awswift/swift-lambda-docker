7ab7b777c869e0bd1fa6fd370d1ec6e9f69f4a5d
Aidan Steele <aidan.steele@glassechidna.com.au>
2017-01-01 22:41:12 +1100

    No need to copy intermediate tgz to S3

--- src/packer.sh
+++ src/packer.sh
@@ -13,7 +13,7 @@ docker build -t awswift/swift-deps:$TAG base
 docker build -t awswift/swift-compiler:$TAG compiler
 docker run --cidfile=cid awswift/swift-compiler:$TAG true
 CID=$(cat cid)
-docker cp $CID:/swiftbuild/package/swift-linux-x86_64-amzn.tgz .
+docker cp $CID:/swiftbuild/package/swift-linux-x86_64-amzn.tgz swiftda/swift-linux-x86_64-amzn.tgz
 docker rm $CID
 
 docker build -t awswift/swiftda:$TAG swiftda
